58680a7ca95ed42d45dd5ff7f3303135797a0db4
Hong Pham Diaz <hongpham2299@gmail.com>
2023-06-11 17:06:42 -0400

    Homework 15

--- src/main/java/eventManagement/xmlParser/Main.java
+++ src/main/java/eventManagement/xmlParser/Main.java
@@ -8,8 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         Logger logger = LogManager.getLogger(Main.class.getName());
+        String xsdPath = "src/main/java/eventManagement/xmlResources/event.xsd";
+        String xmlPath = "src/main/java/eventManagement/xmlResources/event.xml";
 
         DOMParser domParser = new DOMParser();
+
+        domParser.validateXMLSchema(xsdPath, xmlPath);
 
         domParser.getParticipantById("SF-102");
 
