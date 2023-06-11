fca8876a47a2f49719e32eda6b38b73cf0826848
Hong Pham Diaz <hongpham2299@gmail.com>
2023-06-11 17:06:34 -0400

    Homework 15

--- src/main/java/eventManagement/xmlParser/DOMParser.java
+++ src/main/java/eventManagement/xmlParser/DOMParser.java
@@ -9,9 +9,15 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
 import java.io.IOException;
 
 public class DOMParser {
@@ -100,6 +106,19 @@ public class DOMParser {
                     }
                 }
             }
+        }
+    }
+
+    public void validateXMLSchema(String xsdPath, String xmlPath) {
+
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+            logger.info("Validation (XML/SXD) is success!!!!!");
+        } catch (IOException | SAXException e) {
+            logger.info("Exception: " + e.getMessage());
         }
     }
 }
